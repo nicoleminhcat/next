5e6eb1c0af8ce425aca74cac47893f58997097f7
Nicole Pham <nicoleminhpham@gmail.com>
2025-09-03 20:39:33 -0700

    Allow app to use

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors https://app.20in3.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
